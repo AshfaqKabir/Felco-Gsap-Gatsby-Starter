464085a0264de80adffb6e9b869101518ecbe43a
unknown <ashfaqkabir@gmail.com>
2020-01-06 20:38:55 +0600

    Footer Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-155394774-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
